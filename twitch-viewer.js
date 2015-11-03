3fcb723ec3bb750c61b3cd95652dadc523d2ebd0
Martin H <n17r4m@gmail.com>
2015-11-03 02:07:24 -0700

    less crash

--- twitch-viewer.js
+++ twitch-viewer.js
@@ -1,23 +1,22 @@
-if (RFB){
+if (typeof RFB !== "undefined"){
+	console.info("RFB", RFB);
 	RFB._rfb_port = 5901;
+} else {
+	console.info("no RFB")
 }
+
+if (typeof rfb !== "undefined"){
+	console.info("rfb", rfb);
+	rfb._rfb_port = 5901;
+}  else {
+	console.info("no rfb")
+}
+
 
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
 }
 
 if (Meteor.isServer) {
