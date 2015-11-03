3a73012979e1ad6072d50df93fa55383b062be70
Martin H <n17r4m@gmail.com>
2015-11-03 03:33:31 -0700

    making pretty

--- twitch-viewer.js
+++ twitch-viewer.js
@@ -4,7 +4,12 @@
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
-
+	Template.bulletinboard.board = function(){
+		return "This is the bulletin board"
+	}
+	
+	T
+	
 }
 
 if (Meteor.isServer) {
