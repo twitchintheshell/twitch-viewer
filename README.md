b393ea6bb389a4f7b0b64e3c4abb2e899c70b7ac
Martin H <n17r4m@gmail.com>
2015-11-03 14:14:24 -0700

    working prototype

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
-Twitch Viewer
+Twitch viewer (for broacasting)
 
 
+Requirements:
+
+Meteor
+noVNC
+
+Usage:
+
+    $ git clone https://github.com/jakozaur/noVNC.git 
+    $ cd noVNC
+    $ ./utils/launch.sh --vnc localhost:5900
+    
+    $ git clone https://github.com/n17r4m/twitch-viewer.git
+    $ cd twitch-viewer
+    $ meteor
+
+Notes:
+
+Meteor runs on port 3000
+MongoDB runs on port 3001
+noVNC runs on port 6080
--- twitch-viewer.js
+++ twitch-viewer.js
@@ -1,19 +1,75 @@
 
-
+var Board = new Meteor.Collection("board");
+var Ticker = new Meteor.Collection("ticker");
+var CommandLog = new Meteor.Collection("commandlog");
+var Ad = new Meteor.Collection("ad");
 
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-	Template.bulletinboard.board = function(){
-		return "This is the bulletin board"
+	
+	var AD_CHANGE_TIME = 30000;
+	
+	window.Board = Board;
+	window.Ticker = Ticker;
+	window.CommandLog = CommandLog;
+	window.Ad = Ad;
+	
+	
+	Template.bulletinboard.helpers({
+		item: function(){ return Board.find() }
+	})
+	
+	
+	Template.ticker.onRendered(function(){
+		setTimeout(function(){
+			$("#ticker marquee").marquee()
+		}, 1000)
+	})
+	
+	
+	Template.ticker.helpers({
+		item: function(){ return Ticker.find() }
+	})
+	
+	
+	Template.commandlog.helpers({
+		item: function(){ return CommandLog.find() }
+	})
+	
+	
+	Template.ad.onRendered(function(){
+		Meteor.setTimeout(function(){
+			Session.set("ad_url", rand_ad())
+		}, 1000);
+	})
+	Template.ad.helpers({
+		ad_url: function(){ return Session.get("ad_url")}
+	})
+	
+	function rand_ad(){
+		var array = Ad.find().fetch();
+		if (array.length > 0){
+			var randomIndex = Math.floor( Math.random() * array.length );
+			return array[randomIndex].url;
+		} else return "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACwAAAAAAQABAAACAkQBADs=";
 	}
 	
-	T
+	Meteor.setInterval(function(){
+		Session.set("ad_url", rand_ad())
+	}, AD_CHANGE_TIME)
+	
+	
+	Template.clock.helpers({
+		time: function(){ return Session.get("time") }
+	})
+	
+	Meteor.setInterval(function(){
+		Session.set("time", moment().format("MMMM Do, h:mm:ss a"));
+	},1000)
 	
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+	Meteor.startup(function () {
+		// nop
+	});
 }
